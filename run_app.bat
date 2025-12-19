7d17ef02e98f6ce528fcead07dc1fd19ca90942e
LR <lr@4d.com>
2025-12-19 15:29:43 +0100

    changed ports

--- run_app.bat
+++ run_app.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Backend...
-start "VideoTools Backend" /D "backend" .venv\Scripts\uvicorn main:app --reload
+start "VideoTools Backend" /D "backend" .venv\Scripts\uvicorn main:app --reload --port 5555
 
 echo Starting Frontend...
 cd frontend
